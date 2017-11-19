8b9ed1d5f620c205bb1f8e5a400d34fcbf7f0333
Tiira Taneli J <taneli.tiira@helsinki.fi>
2017-11-19 23:04:39 +0200

    1

--- Kurssistatistiikka/src/main/java/ohtu/Submission.java
+++ Kurssistatistiikka/src/main/java/ohtu/Submission.java
@@ -2,6 +2,8 @@ package ohtu;
 
 public class Submission {
     private int week;
+    private int[] exercises;
+    private int hours;
 
     public void setWeek(int week) {
         this.week = week;
@@ -11,9 +13,30 @@ public class Submission {
         return week;
     }
 
+    public int getHours() {
+	return hours;
+    }
+
+    public void setHours(int newHours) {
+	hours = newHours;
+    }
+
+    public int[] getExercises() {
+	return exercises;
+    }
+
+    public void setExercises(int[] newEx) {
+	exercises = newEx;
+    }
+
     @Override
     public String toString() {
-        return ""+week;
+        String ret = "week " + week + ": exercises ";
+	for (int i : exercises) {
+	    ret += i + ", ";
+	}
+	ret += "total " + exercises.length + ", done in " + hours + " hours.";
+	return ret;
     }
     
 }
